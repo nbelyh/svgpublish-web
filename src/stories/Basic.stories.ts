2008c6ddf7a39e01f625201a4c193845c33a40c7
Nikolay Belykh <nbelyh@gmail.com>
2023-12-01 23:30:33 +0100

    improve naming

--- src/stories/Basic.stories.ts
+++ src/stories/Basic.stories.ts
@@ -13,7 +13,7 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const EmbedForceCtrl: Story = {
+export const DefaultEmbedWithCtrlWheel: Story = {
   parameters: {
     layout: 'fullscreen',
     controls: { include: ['url', 'enableZoomCtrl']}
