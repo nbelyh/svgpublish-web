30b1bd36f17ce1515f4846f4bb1ade10db0258e4
Nikolay Belykh <nbelyh@hotmail.com>
2025-07-25 20:17:18 +0200

    bump version

--- packages/lib/src/interfaces/IServices.ts
+++ packages/lib/src/interfaces/IServices.ts
@@ -4,7 +4,6 @@ import { IHoverService } from './IHoverService';
 import { ILinksService } from './ILinksService';
 import { ITooltipService } from './ITooltipService';
 import { ISidebarService } from './ISidebarService';
-// import { IBasicService } from './IBasicService';
 
 export interface IServices {
   view?: IViewService;
@@ -13,5 +12,4 @@ export interface IServices {
   hover?: IHoverService;
   tooltip?: ITooltipService;
   sidebar?: ISidebarService;
-  // [key: string]: IBasicService;
 };
